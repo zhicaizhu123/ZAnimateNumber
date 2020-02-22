cc43d3b76fa6a6456341c03d87030dd50fea188c
zhicaizhu123@126.com <zhicaizhu123@126.com>
2020-02-22 18:47:15 +0800

    :bug 优化目录结构和修复bug

--- examples/App.vue
+++ examples/App.vue
@@ -1,19 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <ZAnimateNumber :end="9999" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ZAnimateNumber } from "../packages";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    ZAnimateNumber
   }
-}
+};
 </script>
 
 <style>
